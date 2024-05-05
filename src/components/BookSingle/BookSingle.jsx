d63f34b9fa5197a265bfc1ce535c1df115b285ad
Yosber Guerrero <yosbergc@gmail.com>
2024-05-04 23:19:53 -0400

    Added bookmark button

--- src/components/BookSingle/BookSingle.jsx
+++ src/components/BookSingle/BookSingle.jsx
@@ -1,9 +1,13 @@
 import './booksingle.css'
+import { FaRegBookmark } from "react-icons/fa"
 function BookSingle ({ bookImg, bookName, bookGenre }) {
     return (<article className="BookSingleContainer">
         <img src={bookImg} alt={bookName} />
         <h3>{bookName}</h3>
-        <span className='genre'>{bookGenre}</span>
+        <div>
+            <span className='genre'>{bookGenre}</span>
+            <button><FaRegBookmark size={20} color='white'/></button>
+        </div>
     </article>)
 }
 export { BookSingle }
