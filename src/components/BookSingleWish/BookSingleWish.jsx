a3a0b987d59ace5f31874605c0e07f2d2dd40610
Yosber Guerrero <yosbergc@gmail.com>
2024-05-05 20:51:48 -0400

    Changed some things in the wishlist design

--- src/components/BookSingleWish/BookSingleWish.jsx
+++ src/components/BookSingleWish/BookSingleWish.jsx
@@ -3,8 +3,10 @@ function BookSingleWish ({bookName, bookImage, deleteBookofCart}) {
     return (
         <section className="booksingle-wish">
             <img src={bookImage} alt={bookName} />
-            <h4>{bookName}</h4>
-            <button onClick={deleteBookofCart}>Eliminar de mi lista</button>
+            <section className='booksingle-wish-info'>
+                <h4>{bookName}</h4>
+                <button onClick={deleteBookofCart}>Eliminar de mi lista</button>
+            </section>
         </section>
     )
 }
