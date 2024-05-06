6a8f0ad311e374fc6c88d7b91d2c2a3dce60ecf4
Yosber Guerrero <yosbergc@gmail.com>
2024-05-05 21:40:03 -0400

    Modified hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import './hero.css'
-import HeroImage from '../../assets/img/Ebook-hero-image.png'
 import { Search } from '../Search/Search'
 function Hero () {
     return (
@@ -8,9 +7,6 @@ function Hero () {
                 <h2>Lecturas sin Fronteras: Tu Biblioteca En Línea</h2>
                 <p>Usa nuestro buscador para poder encontrar tu libro favorito en nuestro catálogo de libros.</p>
                 <Search/>
-            </section>
-            <section>
-                <img src={HeroImage} alt='Hero Image'/>
             </section>
         </section>
     )
